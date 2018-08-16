fbee82e164de18a9db05116f7d236080451e874c
Raja Shravan <rajashravan@hotmail.com>
2018-08-16 16:06:28 -0400

    added in stats and moves

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # PokemonEvo
 
-Python app (with Flask framework) that gives you information on the inputted pokemon's evolution chain
+Python app (using the Flask framework) that gives you information on a given Pokemon (like a PokeDex)
 
-Instructions: TBA
+*Note: the API can be a little slow at times and I haven't implemented a cache yet so please be patient*
+
+Instructions: (To be completed)
+
+Download folder and cd into folder.
+```
+python app.py
+```
+go to http://localhost:5000
 
 Made with the open-source Pokeapi: https://pokeapi.co/
 
 Used Bootstrap for styling
+
+![Alt text](images/sc1.png?raw=true "Screenshot")
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, url_for
 from data import Articles
-
-
 import requests
 import json
 app = Flask(__name__)
 
 Articles = Articles()
+
+#Pokemon Object to make sending data to front end easier
+class Pokemon:
+	def __init__(self):
+		self.weight = 0
+		self.stats = {} # dictionary for stats
+		self.moves = [] # list for moves
+
 
 @app.route("/articles")
 def articles():
@@ -15,23 +21,69 @@ def articles():
 
 @app.route("/")
 def noPoke():
-	print("sdf")
-	print(url_for('getEvo', pokemon='squirtle'))
 	return render_template('noPoke.html')
 
 @app.route("/<pokemon>")	
 def getEvo(pokemon):
+
+	#attributes dict to store 
 
 	# get sprite of given pokemon
 	response = requests.get("https://pokeapi.co/api/v2/pokemon/" + pokemon + "/")
 	data = response.json()
 	poke_URL = data["sprites"]["front_default"]
 
+	#build pokemon object
+	poke = Pokemon()
+	poke.weight = data["weight"]
+
+	#build stats dict
+	stats = data["stats"]
+	for stat in stats:
+		#Speed
+		if (stat["stat"]["name"] == "speed"):
+			poke.stats["speed"] = stat["base_stat"]
+		#SpDef
+		if (stat["stat"]["name"] == "special-defense"):
+			poke.stats["special-defense"] = stat["base_stat"]
+		#SpAttack
+		if (stat["stat"]["name"] == "special-attack"):
+			poke.stats["special-attack"] = stat["base_stat"]
+		#Defense
+		if (stat["stat"]["name"] == "defense"):
+			poke.stats["defense"] = stat["base_stat"]
+		#Attack
+		if (stat["stat"]["name"] == "attack"):
+			poke.stats["attack"] = stat["base_stat"]
+		#HP
+		if (stat["stat"]["name"] == "hp"):
+			poke.stats["hp"] = stat["base_stat"]
+
+	#build moves list
+	moves = data["moves"]
+	for move in moves:
+		poke.moves.append(move["move"]["name"])
+
+
 	# Step 1 - get Species of pokemon
 	response = requests.get("https://pokeapi.co/api/v2/pokemon-species/" + pokemon + "/")
 
 	# turn into JSON
 	data = response.json()
+
+	#stats list
+
+
+	#flavor text
+	flavor_text_entries = data["flavor_text_entries"]
+	flavor_text = None
+
+	#get english flavor text
+	for entry in flavor_text_entries:
+		lang = entry["language"]["name"]
+		if (lang == "en"):
+			flavor_text = entry["flavor_text"]
+			break
 
 	# Step 2 - get evolution chain
 	name = data["name"]
@@ -48,38 +100,48 @@ def getEvo(pokemon):
 		evoChain = evoChain["evolves_to"][0]
 
 
-	# get a list of the next evolutions
+	# get a list of the next evolutions, stored as dict (name and url)
 	evo_list = []
-	evo_urls = []
-	# Step 3 - get next evolution
 
 	#check if final/no evolution
+	'''
+	deprecated - code has been updated to be more robust and only have one endpoint
 	if (len(evoChain["evolves_to"]) == 0):
 		print(name + " is already at max evolution")
-		return render_template('evo.html', poke = pokemon.capitalize(), poke_URL = poke_URL)
+		return render_template('evo.html', poke = pokemon.capitalize(), 
+			poke_URL = poke_URL, flavor_text = flavor_text)
+	'''
 
 	#else, get all the evos and sprite links
 	while(len(evoChain["evolves_to"]) != 0): #we know we start with atleast one
-		evo_list.append(evoChain["evolves_to"][0]["species"]["name"]) #evo name
+		evo_name = evoChain["evolves_to"][0]["species"]["name"] #evo name
 		#get sprite url
-		response = requests.get("https://pokeapi.co/api/v2/pokemon/" + evo_list[-1])
+		response = requests.get("https://pokeapi.co/api/v2/pokemon/" + evo_name)
 		data = response.json()
-		evo_urls.append(data["sprites"]["front_default"])
-		print(name + " evolves into " + evo_list[-1] + " with link " + evo_urls[-1])
+		evo_url = data["sprites"]["front_default"]
+		#make dict
+		evo_entry = {}
+		evo_entry["name"] = evo_name.capitalize()
+		evo_entry["url"] = evo_url.capitalize()
+		evo_list.append(evo_entry)
+		print(name + " evolves into " + evo_name + " with link " + evo_url)
 		evoChain = evoChain["evolves_to"][0]
 
 	#evo = evoChain["evolves_to"][0]["species"]["name"]
 	#print(name + " evolves into " + evo)
-	evo = evo_list[0]
 
+	'''
 	# Step 4 - get sprite of next evolution
 	response = requests.get("https://pokeapi.co/api/v2/pokemon/" + evo)
 	data = response.json()
 	evo_URL = data["sprites"]["front_default"]
 	print(evo_URL)
+	'''
 	#return evo
-	return render_template('evo.html', poke = name.capitalize(), evo = evo.capitalize(), 
-		poke_URL = poke_URL, evo_URL = evo_URL, evo_list = evo_list)
+	return render_template('evo.html', poke = poke, name = name.capitalize(), 
+		flavor_text = flavor_text, poke_URL = poke_URL, evo_list = evo_list)
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
